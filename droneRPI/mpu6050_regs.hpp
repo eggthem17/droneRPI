a1d44b95921600e43573227428c21a83de114500
paxmiler <paxmiler@gmail.com>
2020-08-24 14:53:04 +0900

    add MPU6050 sources

---
+++ droneRPI/mpu6050_regs.hpp
@@ -0,0 +1,20 @@
+//
+//  mpu6050_regs.hpp
+//  droneRPI
+//
+//  Created by Martin.Q on 8/24/20.
+//  Copyright © 2020 Martin.Q. All rights reserved.
+//
+
+#ifndef __MPU6050_REGS_HPP__
+#define __MPU6050_REGS_HPP__
+
+#define PWR_MGMT_1 0x68
+#define GYRO_XOUT_H 0x43
+#define GYRO_XOUT_L 0x44
+#define GYRO_YOUT_H 0x45
+#define GYRO_YOUT_L 0x46
+#define GYRO_ZOUT_H 0x47
+#define GYRO_ZOUT_L 0x48
+
+#endif //__MPU6050_REGS_HPP__
