28a926bbac2102aa6ba81afab010ff2a90a62aea
paxmiler <paxmiler@gmail.com>
2020-08-24 15:34:50 +0900

    tested on RPIZERO

---
+++ droneRPI/mpu6050_regs.h
@@ -0,0 +1,19 @@
+//
+//  mpu6050_regs.h
+//
+//  Created by Martin.Q on 8/24/20.
+//  Copyright © 2020 Martin.Q. All rights reserved.
+//
+
+#ifndef __MPU6050_REGS_H__
+#define __MPU6050_REGS_H__
+
+#define PWR_MGMT_1 0x6B
+#define GYRO_XOUT_H 0x43
+#define GYRO_XOUT_L 0x44
+#define GYRO_YOUT_H 0x45
+#define GYRO_YOUT_L 0x46
+#define GYRO_ZOUT_H 0x47
+#define GYRO_ZOUT_L 0x48
+
+#endif //__MPU6050_REGS_H__
